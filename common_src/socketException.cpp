c00e3c29a28d5f88375033caeaead1c7fc1fc568
RiedelNicolas <nariedel99@gmail.com>
2020-11-07 12:21:59 -0300

    genero clase socketException.cpp

---
+++ common_src/socketException.cpp
@@ -0,0 +1,5 @@
+//
+// Created by riedel on 7/11/20.
+//
+
+#include "socketException.h"
---
+++ common_src/socketException.h
@@ -0,0 +1,14 @@
+//
+// Created by riedel on 7/11/20.
+//
+
+#ifndef INC_7542_TALLER_PAGES_SOCKETEXCEPTION_H
+#define INC_7542_TALLER_PAGES_SOCKETEXCEPTION_H
+
+
+class socketException {
+
+};
+
+
+#endif //INC_7542_TALLER_PAGES_SOCKETEXCEPTION_H
