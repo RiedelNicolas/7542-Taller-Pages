46116bd4dad32a221e4d96191fb79f6744fc944b
RiedelNicolas <nariedel99@gmail.com>
2020-11-07 10:32:42 -0300

    Creo la clase Socket.h

---
+++ common_src/Socket.h
@@ -0,0 +1,14 @@
+//
+// Created by riedel on 7/11/20.
+//
+
+#ifndef INC_7542_TALLER_PAGES_SOCKET_H
+#define INC_7542_TALLER_PAGES_SOCKET_H
+
+
+class Socket {
+
+};
+
+
+#endif //INC_7542_TALLER_PAGES_SOCKET_H
