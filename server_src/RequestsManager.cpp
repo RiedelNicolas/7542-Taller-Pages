ed59b28c734fc3be8d2b69e9bbc2ed184b00acf7
RiedelNicolas <nariedel99@gmail.com>
2020-11-13 04:42:26 -0300

    excep general

--- server_src/RequestsManager.cpp
+++ server_src/RequestsManager.cpp
@@ -2,6 +2,7 @@
 // Created by riedel on 8/11/20.
 //
 
+#include <iostream>
 #include "RequestsManager.h"
 
 RequestsManager::RequestsManager(std::string port) {
@@ -13,11 +14,14 @@ void RequestsManager::run() {
 
     while( !(this->done) ) {
         try {
-            int temp = this->peer.acceptOne();
-            this->clients.push_back(  ClientHandler (temp)  );
+            int fd = this->peer.acceptOne();
+            this->clients.emplace_back(  ClientHandler(fd)  );
 
         }catch (std::exception& e) {
+            std::cerr << e.what() << std::endl;
             this->done = true;
+        } catch (...) {
+            std::cerr << "Unknown error"<< std::endl;
         }
     }
 }
