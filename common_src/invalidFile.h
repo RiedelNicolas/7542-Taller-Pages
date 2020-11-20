ae2c832c030023c72b8eb13bd358a5bd20a5b4af
RiedelNicolas <nariedel99@gmail.com>
2020-11-20 11:05:17 -0300

    Agreco clase invalidFile exception

---
+++ common_src/invalidFile.h
@@ -0,0 +1,16 @@
+//
+// Created by riedel on 20/11/20.
+//
+
+#ifndef INC_7542_TALLER_PAGES_INVALIDFILE_H
+#define INC_7542_TALLER_PAGES_INVALIDFILE_H
+
+
+#include <exception>
+
+class invalidFile: public std::exception {
+
+};
+
+
+#endif //INC_7542_TALLER_PAGES_INVALIDFILE_H
