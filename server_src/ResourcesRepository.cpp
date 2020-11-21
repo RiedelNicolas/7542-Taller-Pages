d74edf807fad5e21ed50d3688c691320dda45e90
RiedelNicolas <nariedel99@gmail.com>
2020-11-21 18:12:40 -0300

    agrego implementaciones de resources repository

--- server_src/ResourcesRepository.cpp
+++ server_src/ResourcesRepository.cpp
@@ -2,16 +2,27 @@
 // Created by riedel on 21/11/20.
 //
 
+#include <fstream>
+#include <sstream>
 #include "ResourcesRepository.h"
+#include "../common_src/InvalidFileException.h"
 
-ResourcesRepository::ResourcesRepository(const std::string &root) : root(root) {}
+ResourcesRepository::ResourcesRepository(const std::string& rootPath) {
+    std::ifstream file(rootPath);
+    if( !file.is_open() ){
+        throw InvalidFileException("unable to open rootFile");
+    }
+    std::stringstream sstr;
+    while(file >> sstr.rdbuf());
+    this->root = sstr.str();
+}
 
 const std::string &ResourcesRepository::getRoot() {
     return root;
 }
 
 const std::string &ResourcesRepository::get(const std::string &key) {
-    this->map
+    this->map[];
 }
 
 
