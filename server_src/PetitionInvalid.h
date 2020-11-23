290c03309046a4063b2d66f6b7d5578fa6f51b76
RiedelNicolas <nariedel99@gmail.com>
2020-11-22 22:47:12 -0300

    agrego firmas de las funciones hijas

--- server_src/PetitionInvalid.h
+++ server_src/PetitionInvalid.h
@@ -9,7 +9,12 @@
 #include "PetitionLogic.h"
 
 class PetitionInvalid  : public PetitionLogic  {
-
+private:
+    std::stringstream petition;
+public:
+    void execute() override;
+    std::string getAnswer() override;
+    std::string getDisplay() override;
 };
 
 
--- server_src/PetitionPostRoot.h
+++ server_src/PetitionPostRoot.h
@@ -9,7 +9,12 @@
 #include "PetitionLogic.h"
 
 class PetitionPostRoot : public PetitionLogic  {
-
+private:
+    std::stringstream petition;
+public:
+    void execute() override;
+    std::string getAnswer() override;
+    std::string getDisplay() override;
 };
 
 
