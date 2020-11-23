521c7af72d73b62e93eabd5dc1ffaf4150b75972
RiedelNicolas <nariedel99@gmail.com>
2020-11-23 14:08:55 -0300

    agrego prefijos a las respuestas

--- server_src/PetitionGet.cpp
+++ server_src/PetitionGet.cpp
@@ -4,10 +4,21 @@
 
 #include "PetitionGet.h"
 
+#define
 
 
 void PetitionGet::execute() {
-    this->parser.get
+    std::string resource = parser.getResource();
+    answer = "HTTP/1.1 200 OK\n\n";
+    if(resource.length() == 0 ){
+        answer.append( resources.getRoot() );
+    }else{
+        try {
+            answer.append( resources.get(resource) );
+        } catch (const std::out_of_range& oor) {
+         answer = "HTTP/1.1 403 FORBIDDEN\n\n";
+        }
+    }
 }
 
 
