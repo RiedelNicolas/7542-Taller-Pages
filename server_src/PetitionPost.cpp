5a834f1ae0a70429ee7a0b5c9041c6079e918cac
RiedelNicolas <nariedel99@gmail.com>
2020-11-23 14:18:59 -0300

    implemento petition post

--- server_src/PetitionPost.cpp
+++ server_src/PetitionPost.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "PetitionPost.h"
+
+void PetitionPost::execute() {
+    std::string resource = parser.getResource();
+    if (resource.size() == 0 ) {
+        answer = "HTTP/1.1 403 FORBIDDEN\n\n";
+    } else {
+        std::string body = parser.getBody();
+        resources.post(resource, body);
+        answer = "HTTP/1.1 403 FORBIDDEN\n\n";
+        answer.append(body);
+    }
+}
--- server_src/PetitionPost.h
+++ server_src/PetitionPost.h
@@ -13,8 +13,6 @@ private:
     std::stringstream petition;
 public:
     void execute() override;
-    std::string getAnswer() override;
-    std::string getDisplay() override;
 };
 
 
