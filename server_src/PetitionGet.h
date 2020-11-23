debc3710d3676cd6f6dc6c9270a797f6d8127fe8
RiedelNicolas <nariedel99@gmail.com>
2020-11-22 22:43:06 -0300

    Arreglo nombre

--- server_src/PetitionGet.h
+++ server_src/PetitionGet.h
@@ -9,7 +9,12 @@
 #include "PetitionLogic.h"
 
 class PetitionGet : public PetitionLogic {
-
+private:
+    std::stringstream petition;
+public:
+    void execute() override;
+    std::string getAnswer() override;
+    std::string getDisplay() override;
 };
 
 
