d03cf300c8bd89301e8c035c60ff4a7608bdb04d
RiedelNicolas <nariedel99@gmail.com>
2020-11-24 00:34:50 -0300

    cpplint

--- server_src/ClientHandler.cpp
+++ server_src/ClientHandler.cpp
@@ -3,12 +3,14 @@
 //
 
 #include <iostream>
+#include <string>
+#include <utility>
 #include "ClientHandler.h"
 
 void ClientHandler::run() {
-    PetitionProcessor petProcessor(resources,this->receivePetition());
+    PetitionProcessor petProcessor(resources, this->receivePetition());
     petProcessor.process();
-    printer.print( petProcessor.getDisplay() );
+    printer.print(petProcessor.getDisplay());
     this->sendResult(petProcessor.getAnswer());
     socket.endWriting();
     this->stop();
@@ -37,6 +39,7 @@ ClientHandler::ClientHandler(Socket&& peer, PrintMonitor &printer,
                              ResourcesRepository &resources):
                              socket(std::move(peer)),
                              printer(printer),
-                             resources(resources){
+                             resources(resources) {
     this->running = true;
 }
+
