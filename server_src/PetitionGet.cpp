6355791e12b6d5d3eda46e14afc3c9196022d312
RiedelNicolas <nariedel99@gmail.com>
2020-11-24 02:42:32 -0300

    getroot

--- server_src/PetitionGet.cpp
+++ server_src/PetitionGet.cpp
@@ -7,14 +7,16 @@
 
 void PetitionGet::execute() {
     std::string resource = parser.getResource();
-    answer = "HTTP/1.1 200 OK\n\n";
+    answer = "HTTP/1.1 200 OK\n";
     if (resource.length() == 0) {
+        answer.append("Content-Type: text/html\n\n");
         answer.append(resources.getRoot());
     } else {
         try {
+            answer.append("\n");
             answer.append(resources.get(resource));
         } catch (const std::out_of_range& oor) {
-         answer = "HTTP/1.1 403 FORBIDDEN\n\n";
+         answer = "HTTP/1.1 404 NOT FOUND\n\n";
         }
     }
 }
